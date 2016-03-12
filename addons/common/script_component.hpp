be53c45f791a286abcd92c9eb611594c9e696a7d
robtherad <robtherad@gmail.com>
2016-03-12 13:19:30 -0500

    Update script_components
    [ci skip]

--- addons/common/script_component.hpp
+++ addons/common/script_component.hpp
@@ -1,3 +1,8 @@
 #define COMPONENT common
 #include "\y\bc\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+
 #include "\y\bc\addons\main\script_macros.hpp"
