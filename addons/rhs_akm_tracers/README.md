2bd983465e56667e01fcef2c18cd6dfe23c0a3fb
robtherad <robtherad@gmail.com>
2016-03-20 01:20:32 -0400

    Rename akm_tracers module
    Add rhs tag to front of module name. From now on if a module is going to
    interfere with or require another mod it should be tagged with that
    mod's name.

--- addons/rhs_akm_tracers/README.md
+++ addons/rhs_akm_tracers/README.md
@@ -1,3 +1,3 @@
-## akm_tracers.pbo
+## rhs_akm_tracers.pbo
 
 Makes changes to RHS config for 7.62x39 AK tracer magazines so that they actually fire tracer rounds.
--- addons/rhs_akm_tracers/script_component.hpp
+++ addons/rhs_akm_tracers/script_component.hpp
@@ -1,3 +1,3 @@
-#define COMPONENT akm_tracers
+#define COMPONENT rhs_akm_tracers
 #include "\y\bc\addons\main\script_mod.hpp"
 #include "\y\bc\addons\main\script_macros.hpp"
